c40390b7f34f2de8499b00734fcad9f9b9d5b44a
bhavyacodes <xbhavyatomar@gmail.com>
2025-12-13 22:46:12 +0530

    feat(auth): implement user model with roles and schema definition

---
+++ auth/src/models/user.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+enum UserRoles {
+  ADMIN = "admin",
+  USER = "user",
+}
+
+interface UserAttrs {
+  email: string;
+  picture?: string;
+  role?: UserRoles;
+}
+
+interface UserDoc extends mongoose.Document {
+  id: mongoose.Types.ObjectId;
+  email: string;
+  picture?: string;
+  role: UserRoles;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    picture: { type: String },
+    role: {
+      type: String,
+      enum: Object.values(UserRoles),
+      default: UserRoles.USER,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform: (doc, ret: any) => {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User, UserRoles };
