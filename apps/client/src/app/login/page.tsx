208f6492dbb2e6cf722af7b541b96607252cd78f
bhavyacodes <xbhavyatomar@gmail.com>
2025-12-17 14:00:41 +0530

    feat(auth): refactor auth service structure and integrate Hono client

--- apps/client/src/app/login/page.tsx
+++ apps/client/src/app/login/page.tsx
@@ -2,8 +2,16 @@
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { UseGoogleLogin } from "./useGoogleLogin";
+import { authClient } from "@booking/auth/app";
+import { useEffect } from "react";
 
 const Page = () => {
+  useEffect(() => {
+    authClient.api.auth["current-user"].$get().then(async (response) => {
+      console.log("Current User:", await response.json());
+    });
+  }, []);
+
   return (
     <div>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
