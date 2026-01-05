ce29e4ff14a1418b9df1a7f69916bd3327d3ff33
bhavyacodes <xbhavyatomar@gmail.com>
2026-01-06 03:12:16 +0530

    refactor(common): replace custom logger implementation with pino and remove unused common logger

--- apps/tickets/src/logger.ts
+++ apps/tickets/src/logger.ts
@@ -1,5 +1,18 @@
-import { getPinoLogger } from "@booking/common";
+import pino = require("pino");
 
-const pinoLogger = getPinoLogger("trace");
+const pinoLogger = pino({
+  level: "trace",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+
+  timestamp: pino.stdTimeFunctions.epochTime,
+  serializers: {
+    error: pino.stdSerializers.err,
+  },
+});
 
 export { pinoLogger as pl };
