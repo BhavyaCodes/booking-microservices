a903a9d05deeed0fc9b775fd930f1715152c7830
bhavyacodes <xbhavyatomar@gmail.com>
2026-01-08 23:46:28 +0530

    fix(common): replace requireAuth response with CustomErrorResponse for unauthenticated access

--- packages/common/middlewares/requireAuth.ts
+++ packages/common/middlewares/requireAuth.ts
@@ -1,20 +1,16 @@
 import type { Context, Next } from "hono";
 import { HTTPException } from "hono/http-exception";
 import type { CurrentUser } from "../interfaces";
+import { CustomErrorResponse, ErrorCodes } from "../error/CustomErrorResponse";
 
 export const requireAuth = async (c: Context, next: Next) => {
   const currentUserId = (c.get("currentUser") as CurrentUser | undefined)?.id;
   if (!currentUserId) {
     throw new HTTPException(401, {
-      res: new Response(
-        JSON.stringify({
-          message: "Not authenticated",
-        }),
-        {
-          status: 401,
-          headers: { "Content-Type": "application/json" },
-        },
-      ),
+      res: new CustomErrorResponse({
+        message: "not authenticated",
+        code: ErrorCodes.INVALID_SESSION,
+      }),
     });
   }
 
