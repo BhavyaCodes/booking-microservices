0ea9bd6773722e22baf37c38344067d875a3bffe
bhavyacodes <xbhavyatomar@gmail.com>
2026-01-16 01:58:59 +0530

    feat(orders): add GIN index on ticket_ids and update schema for orders table

--- apps/orders/src/db/schema.ts
+++ apps/orders/src/db/schema.ts
@@ -5,6 +5,7 @@ import {
   timestamp,
   text,
   pgEnum,
+  index,
 } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 
@@ -20,16 +21,20 @@ export const orderStatusEnum = pgEnum(
   Object.values(OrderStatus) as [string, ...string[]],
 );
 
-export const ordersTable = pgTable("orders", {
-  id: uuid()
-    .primaryKey()
-    .default(sql`uuidv7()`),
-  userId: uuid().notNull(),
-  amount: integer().notNull(),
-  status: orderStatusEnum()
-    .notNull()
-    .default(OrderStatus.CREATED)
-    .$type<OrderStatus>(),
-  expiresAt: timestamp().notNull(),
-  ticketIds: text("ticket_ids").array().notNull(),
-});
+export const ordersTable = pgTable(
+  "orders",
+  {
+    id: uuid()
+      .primaryKey()
+      .default(sql`uuidv7()`),
+    userId: uuid().notNull(),
+    amount: integer().notNull(),
+    status: orderStatusEnum()
+      .notNull()
+      .default(OrderStatus.CREATED)
+      .$type<OrderStatus>(),
+    expiresAt: timestamp().notNull(),
+    ticketIds: text("ticket_ids").array().notNull(),
+  },
+  (table) => [index("ticket_ids_idx").using("gin", table.ticketIds)],
+);
