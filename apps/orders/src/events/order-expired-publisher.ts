164b7aae0ea8bcc2cbe74a03714cf1b9f052a53e
bhavyacodes <xbhavyatomar@gmail.com>
2026-02-21 22:41:35 +0530

    feat(orders): add orderExpiredPublisher and integrate with expiration queue

---
+++ apps/orders/src/events/order-expired-publisher.ts
@@ -0,0 +1,8 @@
+import { NATSEvent, OrderExpiredEvent, Subjects } from "@booking/common";
+import { natsWrapper } from "../nats-wrapper";
+
+export const orderExpiredPublisher = async (
+  data: OrderExpiredEvent["data"],
+) => {
+  return natsWrapper.js.publish(Subjects.OrderExpired, JSON.stringify(data));
+};
--- apps/orders/src/queues/expiration-queue.ts
+++ apps/orders/src/queues/expiration-queue.ts
@@ -1,14 +1,16 @@
 import Bull = require("bull");
 import { pl } from "../logger";
 
-const expirationQueue = new Bull<{ orderId: string; ticketIds: string[] }>(
-  "order:expiration",
-  {
-    redis: {
-      host: process.env.REDIS_HOST,
-    },
+import { OrderExpiredEvent, Subjects } from "@booking/common";
+import { orderExpiredPublisher } from "../events/order-expired-publisher";
+
+const BULL_QUEUE_NAME = "order:expiration";
+
+const expirationQueue = new Bull<OrderExpiredEvent["data"]>(BULL_QUEUE_NAME, {
+  redis: {
+    host: process.env.REDIS_HOST,
   },
-);
+});
 
 expirationQueue.on("error", (err) => {
   pl.error(err, "Error in expiration queue");
@@ -18,15 +20,22 @@ expirationQueue.whenCurrentJobsFinished().then(() => {
   pl.info("All current jobs in expiration queue have finished");
 });
 
-expirationQueue.process(async (job: Bull.Job<{ orderId?: string }>) => {
+expirationQueue.process(async (job, done) => {
   pl.info(
     {
       jobId: job.id,
       orderId: job.data?.orderId,
+      ticketIds: job.data?.ticketIds,
       queueName: job.queue.name,
     },
     "Processing expiration job",
   );
+
+  const pa = await orderExpiredPublisher(job.data).catch((err) => {
+    done(err);
+  });
+
+  done(null, pa);
 });
 
 export { expirationQueue };
