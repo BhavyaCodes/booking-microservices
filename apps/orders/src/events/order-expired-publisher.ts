28ccfb8f9c3223e71f2858be29e3c1874577f6bc
bhavyacodes <xbhavyatomar@gmail.com>
2026-03-26 02:03:46 +0530

    chore(orders): remove unused order expired publisher file

--- apps/orders/src/events/order-expired-publisher.ts
+++
@@ -1,8 +0,0 @@
-import { OrderExpiredEvent, Subjects } from "@booking/common";
-import { natsWrapper } from "../nats-wrapper";
-
-export const orderExpiredPublisher = async (
-  data: OrderExpiredEvent["data"],
-) => {
-  return natsWrapper.js.publish(Subjects.OrderExpired, JSON.stringify(data));
-};
