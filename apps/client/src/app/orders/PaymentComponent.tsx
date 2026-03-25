7e301d8a717b27d0cc35d3fbe75037475f834468
bhavyacodes <xbhavyatomar@gmail.com>
2026-03-26 00:46:52 +0530

    feat(client): enhance PaymentComponent with order cancellation functionality and pass orderId prop

--- apps/client/src/app/orders/PaymentComponent.tsx
+++ apps/client/src/app/orders/PaymentComponent.tsx
@@ -3,31 +3,80 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useState } from "react";
 import CheckoutForm from "./CheckoutForm";
+import { hc } from "@booking/auth/client";
+import { OrdersAppType } from "@booking/orders/client";
 const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
 );
 
-const PaymentComponent = ({ clientSecret }: { clientSecret: string }) => {
+const PaymentComponent = ({
+  clientSecret,
+  orderId,
+}: {
+  clientSecret: string;
+  orderId: string;
+}) => {
   const [paymentStatus, setPaymentStatus] = useState<
     "notSubmitted" | "success" | "failed"
   >("notSubmitted");
 
+  const [isCancelling, setIsCancelling] = useState(false);
+  // const cookieHeader = cookieStore.toString();
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  const ordersClient = hc<OrdersAppType>(
+    "https://booking.dev",
+    {
+      headers: {
+        // Cookie: cookieHeader,
+        Host: "booking.dev",
+      },
+    },
+  );
+
+  const handleCancelOrder = async () => {
+    setIsCancelling(true);
+    await ordersClient.api.orders[":orderId"].cancel
+      .$post({
+        param: {
+          orderId,
+        },
+      })
+      .then(() => {
+        setIsCancelled(true);
+      })
+      .finally(() => {
+        setIsCancelling(false);
+      });
+  };
+
   return (
     <div className="App">
       <h2>Complete your payment</h2>
-      <Elements
-        stripe={stripePromise}
-        options={{
-          clientSecret,
-          appearance: {
-            theme: "night",
-          },
-        }}
-      >
-        {clientSecret && paymentStatus === "notSubmitted" && (
-          <CheckoutForm setPaymentStatus={setPaymentStatus} />
-        )}
-      </Elements>
+      {isCancelled ? (
+        <p>Order Cancelled</p>
+      ) : (
+        <>
+          <p>Order ID: {orderId}</p>
+          <Elements
+            stripe={stripePromise}
+            options={{
+              clientSecret,
+              appearance: {
+                theme: "night",
+              },
+            }}
+          >
+            {clientSecret && paymentStatus === "notSubmitted" && (
+              <CheckoutForm setPaymentStatus={setPaymentStatus} />
+            )}
+          </Elements>
+        </>
+      )}
+
+      <button type="button" onClick={handleCancelOrder}>
+        cancel order
+      </button>
     </div>
   );
 };
