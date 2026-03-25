608cac2fd933c39d3ccae9fe9aad12ab1fc1ca31
bhavyacodes <xbhavyatomar@gmail.com>
2026-03-26 00:48:05 +0530

    feat(utils): extend pod deletion script to include auth pods

--- utils/scripts/delete-tickets-orders-pods.sh
+++ utils/scripts/delete-tickets-orders-pods.sh
@@ -10,4 +10,7 @@ kubectl delete pods -l app=tickets
 echo "Deleting pods for orders..."
 kubectl delete pods -l app=orders
 
+echo "Deleting pods for auth..."
+kubectl delete pods -l app=auth
+
 echo "Pods deletion completed."
